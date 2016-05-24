1343f933bb07a0594ebef512ca22c2b014e348d9
Cypher <Cypher@github>
2016-05-24 02:44:09 +0200

    Xglyph v13, added COMPLEX/SIMPLE command glyph hooking

--- Xglyph/app/src/main/java/com/xingress/xglyph/MainActivity.java
+++ Xglyph/app/src/main/java/com/xingress/xglyph/MainActivity.java
@@ -3,6 +3,8 @@ package com.xingress.xglyph;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,13 @@ import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created by Cypher on 15/10/28.
@@ -27,6 +35,8 @@ public class MainActivity extends Activity {
 	public static final String NORMALHACKKEY = "Xglyph_NormalHackKey";
 	public static final String GLYPHHACK = "Xglyph_GlyphHack";
 	public static final String GLYPHHACKKEY = "Xglyph_GlyphHackKey";
+	public static final String GLYPHHACKSPEED = "Xglyph_GlyphHackSpeed";
+	public static final String GLYPHHACKSPEEDFAST = "Xglyph_GlyphHackSpeedFast";
 
 	private SharedPreferences pref;
 	private Switch switch_activate;
@@ -34,12 +44,16 @@ public class MainActivity extends Activity {
 	private CheckBox cb_debugLog;
 	private CheckBox cb_normalHack;
 	private CheckBox cb_glyphHack;
+	private CheckBox cb_glyphHackSpeed;
 	private RadioGroup rg_normal;
 	private RadioGroup rg_glyph;
+	private RadioGroup rg_glyphSpeed;
 	private RadioButton rb_normal_key;
 	private RadioButton rb_normal_noKey;
 	private RadioButton rb_glyph_key;
 	private RadioButton rb_glyph_noKey;
+	private RadioButton rb_glyph_fast;
+	private RadioButton rb_glyph_slow;
 	private Button button_description;
 	private boolean showToast;
 
@@ -55,17 +69,23 @@ public class MainActivity extends Activity {
 		cb_replaceGlyphs = (CheckBox) findViewById(R.id.cb_replaceGlyphs);
 		cb_normalHack = (CheckBox) findViewById(R.id.cb_normalHack);
 		cb_glyphHack = (CheckBox) findViewById(R.id.cb_glyphHack);
+		cb_glyphHackSpeed = (CheckBox) findViewById(R.id.cb_glyphHackSpeed);
 		rg_normal = (RadioGroup) findViewById(R.id.rg_normal);
 		rg_glyph = (RadioGroup) findViewById(R.id.rg_glyph);
+		rg_glyphSpeed = (RadioGroup) findViewById(R.id.rg_glyphSpeed);
 		rb_normal_key = (RadioButton) findViewById(R.id.rb_normal_key);
 		rb_normal_noKey = (RadioButton) findViewById(R.id.rb_normal_noKey);
 		rb_glyph_key = (RadioButton) findViewById(R.id.rb_glyph_key);
 		rb_glyph_noKey = (RadioButton) findViewById(R.id.rb_glyph_noKey);
+		rb_glyph_fast = (RadioButton) findViewById(R.id.rb_glyph_fast);
+		rb_glyph_slow = (RadioButton) findViewById(R.id.rb_glyph_slow);
 
 		rb_normal_key.setEnabled(false);
 		rb_normal_noKey.setEnabled(false);
 		rb_glyph_key.setEnabled(false);
 		rb_glyph_noKey.setEnabled(false);
+		rb_glyph_fast.setEnabled(false);
+		rb_glyph_slow.setEnabled(false);
 
 		button_description = (Button) findViewById(R.id.button_description);
 
@@ -96,6 +116,14 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		cb_glyphHackSpeed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				rb_glyph_fast.setEnabled(isChecked);
+				rb_glyph_slow.setEnabled(isChecked);
+			}
+		});
+
 		button_description.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -114,6 +142,23 @@ public class MainActivity extends Activity {
 				});
 			}
 		});
+
+		try {
+			String version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+
+			ZipFile zf = new ZipFile(getPackageManager().getApplicationInfo(getPackageName(), 0).sourceDir);
+			ZipEntry ze = zf.getEntry("classes.dex");
+			long time = ze.getTime();
+			String buildDate = SimpleDateFormat.getInstance().format(new java.util.Date(time));
+
+			Resources res = getResources();
+			String text = String.format(res.getString(R.string.credits), version, buildDate);
+
+			TextView tv_credits = (TextView) findViewById(R.id.tv_credits);
+			tv_credits.setText(text);
+
+			zf.close();
+		} catch (PackageManager.NameNotFoundException | IOException ignored) { }
 	}
 
 	@Override
@@ -127,10 +172,13 @@ public class MainActivity extends Activity {
 		cb_replaceGlyphs.setChecked(pref.getBoolean(REPLACEGLYPHS, false));
 		cb_normalHack.setChecked(pref.getBoolean(NORMALHACK, false));
 		cb_glyphHack.setChecked(pref.getBoolean(GLYPHHACK, false));
+		cb_glyphHackSpeed.setChecked(pref.getBoolean(GLYPHHACKSPEED, false));
 		rb_normal_key.setChecked(pref.getBoolean(NORMALHACKKEY, true));
 		rb_normal_noKey.setChecked(!pref.getBoolean(NORMALHACKKEY, true));
 		rb_glyph_key.setChecked(pref.getBoolean(GLYPHHACKKEY, true));
 		rb_glyph_noKey.setChecked(!pref.getBoolean(GLYPHHACKKEY, true));
+		rb_glyph_fast.setChecked(pref.getBoolean(GLYPHHACKSPEEDFAST, true));
+		rb_glyph_slow.setChecked(!pref.getBoolean(GLYPHHACKSPEEDFAST, true));
 
 		showToast = true;
 	}
@@ -146,6 +194,7 @@ public class MainActivity extends Activity {
 		pref.edit().putBoolean(REPLACEGLYPHS, cb_replaceGlyphs.isChecked()).apply();
 		pref.edit().putBoolean(NORMALHACK, cb_normalHack.isChecked()).apply();
 		pref.edit().putBoolean(GLYPHHACK, cb_glyphHack.isChecked()).apply();
+		pref.edit().putBoolean(GLYPHHACKSPEED, cb_glyphHackSpeed.isChecked()).apply();
 
 		int i = rg_normal.getCheckedRadioButtonId();
 
@@ -161,6 +210,14 @@ public class MainActivity extends Activity {
 			pref.edit().putBoolean(GLYPHHACKKEY, true).apply();
 		} else if (i == R.id.rb_glyph_noKey) {
 			pref.edit().putBoolean(GLYPHHACKKEY, false).apply();
+		}
+
+		i = rg_glyphSpeed.getCheckedRadioButtonId();
+
+		if (i == R.id.rb_glyph_fast) {
+			pref.edit().putBoolean(GLYPHHACKSPEEDFAST, true).apply();
+		} else if (i == R.id.rb_glyph_slow) {
+			pref.edit().putBoolean(GLYPHHACKSPEEDFAST, false).apply();
 		}
 
 		showToast = true;
--- Xglyph/app/src/main/java/com/xingress/xglyph/Xglyph.java
+++ Xglyph/app/src/main/java/com/xingress/xglyph/Xglyph.java
@@ -23,11 +23,14 @@ import static de.robv.android.xposed.XposedHelpers.newInstance;
 /**
  * Created by xfunx on 15/8/25.
  * Patched by Cypher on 15/10/28.
+ *   last updated on 16/05/24.
  */
 public class Xglyph implements IXposedHookLoadPackage {
 	private static final String TAG = Xglyph.class.getSimpleName() + ": ";
 
 	final XSharedPreferences pref = new XSharedPreferences(Xglyph.class.getPackage().getName(), MainActivity.PREF);
+
+	private static boolean glyphSpeedTriggered = false;
 
 	private void debugLog(String message) {
 		pref.reload();
@@ -48,14 +51,29 @@ public class Xglyph implements IXposedHookLoadPackage {
 		pref.reload();
 
 		if (pref.getBoolean(MainActivity.ACTIVATE, false)) {
+			final String moreGlyph1 = "ikj";
+			final String moreGlyph2 = "jki";
+			final String lessGlyph1 = "gkh";
+			final String lessGlyph2 = "hkg";
+			final String complexGlyph1 = "jkgh";
+			final String complexGlyph2 = "hgkj";
+			final String simpleGlyph1 = "ji";
+			final String simpleGlyph2 = "ij";
+
 			final String portalHackingParamsClassName = "com.nianticproject.ingress.shared.rpc.PortalHackingParams";
 			final String userInputGlyphSequenceClassName = "com.nianticproject.ingress.glyph.UserInputGlyphSequence";
 			final String glyphClassName = "com.nianticproject.ingress.glyph.Glyph";
 			final String turingClassName = "com.nianticproject.ingress.common.utility.Turing";
+			final String turingClassMethodName1 = "g";
+			final String turingClassMethodName2 = "l";
+			final String mqClassName = "o.mq"; // FIXME: this might change for new versions of the Ingress client
+			final String mqClassMethodName = "ˊ"; // FIXME: this might change for new versions of the Ingress client
+
 			final Class<?> portalHackingParamsClass;
 			final Class<?> userInputGlyphSequenceClass;
 			final Class<?> glyphClass;
 			final Class<?> turingClass;
+			final Class<?> mqClass;
 
 			try {
 				portalHackingParamsClass = findClass(portalHackingParamsClassName, lpparam.classLoader);
@@ -82,6 +100,13 @@ public class Xglyph implements IXposedHookLoadPackage {
 				turingClass = findClass(turingClassName, lpparam.classLoader);
 			} catch (XposedHelpers.ClassNotFoundError e) {
 				debugLog(turingClassName + ": ClassNotFoundError");
+				return;
+			}
+
+			try {
+				mqClass = findClass(mqClassName, lpparam.classLoader);
+			} catch (XposedHelpers.ClassNotFoundError e) {
+				debugLog(mqClassName + ": ClassNotFoundError");
 				return;
 			}
 
@@ -118,6 +143,8 @@ public class Xglyph implements IXposedHookLoadPackage {
 					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 						debugLog("PortalHackingParams: constructor with (String, UserInputGlyphSequence, UserInputGlyphSequence) called");
 
+						pref.reload();
+
 						String uigs1 = "null";
 						String uigs2 = "null";
 
@@ -125,8 +152,6 @@ public class Xglyph implements IXposedHookLoadPackage {
 							uigs1 = param.args[1].toString();
 
 							debugLog("original uigs1: " + uigs1);
-
-							pref.reload();
 
 							if (pref.getBoolean(MainActivity.REPLACEGLYPHS, false)) {
 								List<Object> glyphList = new ArrayList<>();
@@ -148,71 +173,95 @@ public class Xglyph implements IXposedHookLoadPackage {
 							debugLog("original uigs1: " + uigs1);
 						}
 
-						boolean hookKeyHack = true;
+						Object commandGlyphKey = null;
+						Object commandGlyphSpeed = null;
 
 						if (param.args[2] != null) {
 							uigs2 = param.args[2].toString();
 
 							debugLog("original uigs2: " + uigs2);
 
-							int glyphStart = uigs2.indexOf("glyphOrder=") + 11;
-							int glyphStop  = uigs2.indexOf("}]");
+							List<String> glyphStringList = filterGlyphStrings(uigs2);
 
-							String glyphString = uigs2.substring(glyphStart, glyphStop);
+							debugLog("command glyph inputs: " + glyphStringList);
 
-							debugLog("command glyph input: " + glyphString);
+							if (glyphStringList.contains(moreGlyph1) || glyphStringList.contains(moreGlyph2)) {
+								commandGlyphKey = newInstance(glyphClass, moreGlyph1);
+							} else if (glyphStringList.contains(lessGlyph1) || glyphStringList.contains(lessGlyph2)) {
+								commandGlyphKey = newInstance(glyphClass, lessGlyph1);
+							}
 
-							switch (glyphString) {
-								case "ikj":
-									hookKeyHack = false;
-									break;
-								case "jki":
-									hookKeyHack = false;
-									break;
-								case "gkh":
-									hookKeyHack = false;
-									break;
-								case "hkg":
-									hookKeyHack = false;
-									break;
+							if (glyphStringList.contains(complexGlyph1) || glyphStringList.contains(complexGlyph2)) {
+								commandGlyphSpeed = newInstance(glyphClass, complexGlyph1);
+							} else if (glyphStringList.contains(simpleGlyph1) || glyphStringList.contains(simpleGlyph2)) {
+								commandGlyphSpeed = newInstance(glyphClass, simpleGlyph1);
 							}
 						} else {
 							debugLog("original uigs2: " + uigs2);
 						}
 
-						if (hookKeyHack) {
-							pref.reload();
-
-							if (pref.getBoolean(MainActivity.GLYPHHACK, false)) {
-								Object commandGlyph = null;
-
+						if (pref.getBoolean(MainActivity.GLYPHHACK, false)) {
+							if (commandGlyphKey == null) {
 								if (pref.getBoolean(MainActivity.GLYPHHACKKEY, true)) {
-									commandGlyph = newInstance(glyphClass, "ikj");
+									commandGlyphKey = newInstance(glyphClass, moreGlyph1);
 									debugLog("Glyph Hack key request set");
 								} else if (!pref.getBoolean(MainActivity.GLYPHHACKKEY, true)) {
-									commandGlyph = newInstance(glyphClass, "gkh");
+									commandGlyphKey = newInstance(glyphClass, lessGlyph1);
 									debugLog("Glyph Hack no key request set");
-								}
-
-								if (commandGlyph != null) {
-									List<Object> glyphList = new ArrayList<>();
-									glyphList.add(commandGlyph);
-
-									int min = 400;
-									int max = 500;
-									Random rand = new Random();
-									long randomNum = (long) rand.nextInt((max - min) + 1) + min;
-
-									Object uigs = newInstance(userInputGlyphSequenceClass, glyphList, false, randomNum);
-									uigs2 = uigs.toString();
-
-									param.args[2] = uigs;
 								}
 							}
 						}
 
-						debugLog("patched uigs1 = " + uigs1);
-						debugLog("patched uigs2 = " + uigs2);
+						if (pref.getBoolean(MainActivity.GLYPHHACKSPEED, false)) {
+							if (commandGlyphSpeed == null) {
+								if (pref.getBoolean(MainActivity.GLYPHHACKSPEEDFAST, true)) {
+									commandGlyphSpeed = newInstance(glyphClass, complexGlyph1);
+									debugLog("Glyph Hack fast set");
+								} else if (!pref.getBoolean(MainActivity.GLYPHHACKSPEEDFAST, true)) {
+									commandGlyphSpeed = newInstance(glyphClass, simpleGlyph1);
+									debugLog("Glyph Hack slow set");
+								}
+							}
+						}
+
+						if (commandGlyphKey != null || commandGlyphSpeed != null) {
+							List<Object> glyphList = new ArrayList<>();
+
+							int min = 0;
+							int max = 0;
+
+							if (commandGlyphKey != null) {
+								glyphList.add(commandGlyphKey);
+
+								min += 400; // average input time for more/less
+								max += 500;
+							}
+
+							if (commandGlyphSpeed != null && glyphSpeedTriggered) {
+								glyphList.add(commandGlyphSpeed);
+
+								if (min != 0) {
+									min += 300; // average time gap between two glyphs
+									max += 300;
+								}
+
+								min += 600; // average input time for complex (simple takes less, but who cares)
+								max += 800;
+
+								glyphSpeedTriggered = false;
+							}
+
+							Random rand = new Random();
+							long randomNum = (long) rand.nextInt((max - min) + 1) + min;
+
+							Object uigs = newInstance(userInputGlyphSequenceClass, glyphList, false, randomNum);
+							uigs2 = uigs.toString();
+
+							param.args[2] = uigs;
+						}
+
+						debugLog("patched uigs1: " + uigs1);
+						debugLog("patched uigs2: " + uigs2);
 					}
 				});
 			} catch (NoSuchMethodError e) {
@@ -220,7 +269,7 @@ public class Xglyph implements IXposedHookLoadPackage {
 			}
 
 			try {
-				findAndHookMethod(turingClass, "g", String.class, new XC_MethodHook() {
+				findAndHookMethod(turingClass, turingClassMethodName1, String.class, new XC_MethodHook() {
 					@Override
 					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 						debugLog("begin Calculate glyph");
@@ -243,18 +292,57 @@ public class Xglyph implements IXposedHookLoadPackage {
 					}
 				});
 			} catch (NoSuchMethodError error) {
-				debugLog("Turing.g: NoSuchMethodError");
+				debugLog(turingClassName + "." + turingClassMethodName1 + ": NoSuchMethodError");
 			}
 
 			try {
-				findAndHookMethod(turingClass, "l", new XC_MethodHook() {
+				findAndHookMethod(turingClass, turingClassMethodName2, new XC_MethodHook() {
 					@Override
 					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 						IngressGlyph.glyphSequence.clear();
 					}
 				});
 			} catch (NoSuchMethodError error) {
-				debugLog("Turing.l: NoSuchMethodError");
+				debugLog(turingClassName + "." + turingClassMethodName2 + ": NoSuchMethodError");
+			}
+
+			try {
+				findAndHookMethod(mqClass, mqClassMethodName, String.class, new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						debugLog("mq." + mqClassMethodName + " called");
+
+						String glyphString = (String) param.args[0];
+
+						debugLog("param[0]: " + glyphString);
+
+						glyphSpeedTriggered = true;
+
+						pref.reload();
+
+						if (pref.getBoolean(MainActivity.GLYPHHACKSPEED, false)) {
+							String commandGlyphString = "";
+
+							if (glyphString.equals(complexGlyph1) || glyphString.equals(complexGlyph2)) {
+								commandGlyphString = complexGlyph1;
+							} else if (glyphString.equals(simpleGlyph1) || glyphString.equals(simpleGlyph2)) {
+								commandGlyphString = simpleGlyph1;
+							}
+
+							if (commandGlyphString.equals("")) {
+								if (pref.getBoolean(MainActivity.GLYPHHACKSPEEDFAST, true)) {
+									param.args[0] = complexGlyph1;
+									debugLog("Glyph Hack fast set");
+								} else if (!pref.getBoolean(MainActivity.GLYPHHACKSPEEDFAST, true)) {
+									param.args[0] = simpleGlyph1;
+									debugLog("Glyph Hack slow set");
+								}
+							}
+						}
+					}
+				});
+			} catch (NoSuchMethodError error) {
+				debugLog(mqClassName + "." + mqClassMethodName + ": NoSuchMethodError");
 			}
 		} else {
 			debugLog("Xglyph switched off");
@@ -317,5 +405,24 @@ public class Xglyph implements IXposedHookLoadPackage {
 		} catch (NoSuchMethodError error) {
 			debugLog("ApplicationPackageManager.getInstalledPackages: NoSuchMethodError");
 		}
+	}
+
+	private List<String> filterGlyphStrings(String glyphSequence) {
+		List<String> glyphStringList = new ArrayList<>();
+
+		int glyphStart;
+		int glyphStop = 0;
+		String glyphString;
+
+		while(!glyphSequence.substring(glyphStop, glyphStop + 2).equals("}]")) {
+			glyphStart = glyphSequence.substring(glyphStop).indexOf("glyphOrder=") + 11 + glyphStop;
+			glyphStop  = glyphSequence.substring(glyphStart).indexOf("}") + glyphStart;
+
+			glyphString = glyphSequence.substring(glyphStart, glyphStop);
+
+			glyphStringList.add(glyphString);
+		}
+
+		return glyphStringList;
 	}
 }
